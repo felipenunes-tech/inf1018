152122653f655ea8dfd232be4266b8cdb93de17e
Felipe Nunes <devbyfn@gmail.com>
2026-03-05 13:35:37 +0000

    Fim do ex2

--- Lab3/ex2.c
+++ Lab3/ex2.c
@@ -3,10 +3,16 @@
 int odd_ones(unsigned int x) {
 
     /* escreva seu código aqui */
-    
-
-
+    int cont = 0;
+    while(x>0){
+      if(x & 1){
+        cont++;
+      }
+      x >>=1;
+    }
+    return cont & 1;
 }
+
 int main() {
   printf("%x tem numero %s de bits\n",0x01010101,odd_ones(0x01010101) ? "impar":"par");
   printf("%x tem numero %s de bits\n",0x01030101,odd_ones(0x01030101) ? "impar":"par");
