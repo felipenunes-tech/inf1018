2c26d6e7bdadaab14285cb369203e1e638e77be3
Felipe Nunes <devbyfn@gmail.com>
2026-03-09 20:06:36 +0000

    Fim do Lab3

--- Lab3/ex3.c
+++ Lab3/ex3.c
@@ -1,23 +1,32 @@
 #include <stdio.h>
 
+//A
 unsigned char switch_byte(unsigned char x){
-
     unsigned char temp,temp2;
     temp = x<<4; // 1011 0000
     temp2 =  x>>4; // 0000 1010      
     x= (temp | temp2);
-    
     return x;
 }
 
+//B
+unsigned char rotate_left(unsigned char x, int n){
+    unsigned char temp,temp2;
+    temp = x<<n;
+    temp2 = x>>(8-n);
+    x = (temp | temp2);
+    return x;
+}
 
 int main(void){
 
     // 0xAB
     // 1010  1011
     // 0xBA
+    printf("%X vira %X\n",0xAB,switch_byte(0xAB));
 
-    printf("%X tem numero %X\n",0xAB,switch_byte(0xAB));
+    printf("%X vira %X\n",0x61,rotate_left(0x61,2));
+
     return 0;
 
 }
