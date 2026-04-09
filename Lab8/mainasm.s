7476bbc67c98fd43f92bc407714bbca53d6b6305
Felipe Nunes <devbyfn@gmail.com>
2026-04-09 11:31:56 +0000

    Ex2 em andamento

--- Lab8/mainasm.s
+++ Lab8/mainasm.s
@@ -1,3 +1,15 @@
+/*
+int main() {
+  int i;
+  int *p = nums;
+  for (i = 0; i < 5; i++) {
+    printf("%d\n", filtro(*p,LIM));
+    p++;
+  }
+  return 0;
+}
+*/
+
 .data
  nums: .int 3, -5, 7, 8, -2
  s1:   .string "%d\n"
@@ -13,8 +25,20 @@
     movq  %r12, -16(%rbp)
 
   /* coloque seu codigo aqui */
+    movq $0, %rbx                # i = 0 (índice inicial)
+    movq $nums, %r12d            # p = nums
+
+Loop:
+    cmpl $5, %rbx # i=5?
+    je Finalizacao
+    movl(%r12), %edi
+
+
+
+
 
   /* finalizacao */
+Finalizacao:
     movq -8(%rbp), %rbx
     movq -16(%rbp), %r12
     leave
